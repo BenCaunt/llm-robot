3bcc1055cf22830fb3ebf14203166cd77f0d2626
Ben Caunt <ben@warmhub.com>
2025-02-24 23:01:46 -0500

    update simlink

--- src/client/constants.py
+++ src/client/constants.py
@@ -1,4 +1,12 @@
+CAMERA_WIDTH = 640
+CAMERA_HEIGHT = 480
+CAMERA_FPS = 30
+CAMERA_FRAME_KEY = "robot/camera/frame"
 
+
+# these two should be false unless you are using a stereo camera
+CROP_TO_MONO = False
+CROP_TO_RIGHT_FRAME = False
 # Zenoh keys
 CAMERA_FRAME_KEY = "robot/camera/frame"
 ROBOT_TWIST_CMD_KEY = "robot/cmd"
